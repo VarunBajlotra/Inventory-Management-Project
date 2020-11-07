540a1506d12416d431247060903fcab8657299ff
VAIBHAV189 <varshney.vaibhav18@gmail.com>
2020-11-07 13:27:13 +0530

    resolving git error

---
+++ public/admin/addproduct/script.js
@@ -0,0 +1,23 @@
+$(()=>{
+    var element = $('#photo')[0]
+    var url
+    element.addEventListener('change', function() { 
+        url = URL.createObjectURL(element.files[0]); 
+    })
+    
+    $('#submit').click(()=>{
+        downloadImage(url)
+    })
+})
+
+function downloadImage(url) {
+    var tag = document.createElement('a')
+    tag.href = url
+    tag.target = '_blank'
+    tag.download = 'product.jpg'
+    document.body.appendChild(tag)
+    tag.click()
+    document.body.removeChild(tag)
+}
+
+    
